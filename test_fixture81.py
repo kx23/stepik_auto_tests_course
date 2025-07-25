c30bb5e2f899ea1e455b24d37c68d064e37f3f2d
kx23 <artwrkml@gmail.com>
2025-07-25 23:11:57 +0300

    add marks

--- test_fixture81.py
+++ test_fixture81.py
@@ -5,25 +5,24 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
-    # этот код выполнится после завершения теста
     print("\nquit browser..")
     browser.quit()
 
 
 class TestMainPage1:
 
-    # вызываем фикстуру в тесте, передав ее как параметр
     @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    @pytest.mark.regression
+    @pytest.mark.smoke
+    @pytest.mark.win10
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
--- test_marks_xfail.py
+++ test_marks_xfail.py
@@ -5,25 +5,27 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
-    # этот код выполнится после завершения теста
     print("\nquit browser..")
     browser.quit()
 
 
 class TestMainPage1:
 
-    # вызываем фикстуру в тесте, передав ее как параметр
-    @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    @pytest.mark.regression
+    @pytest.mark.xfail  # проходит, но xpass
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail  # фейлится, xfail
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "button.favorite")
